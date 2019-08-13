cee7a076e34ec85e85dc5759e68433a9b55f4a43
Vince Carollo <vincecarollo@gmail.com>
2019-08-13 08:49:01 -0600

    Extract user creation to users controller

---
+++ controllers/api/v1/users_controller.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const { check, validationResult } = require('express-validator');
+const uuidv4 = require('uuid/v4');
+var User = require('../../../models').User;
+
+module.exports = { create: function (req, res, next) {
+    const errors = validationResult(req)
+    if (req.body.password != req.body.password_confirmation) {
+      return res.status(500).json({ errors: 'passwords do not match' })
+    } else if (!errors.isEmpty()) {
+      return res.status(500).json({ errors: errors.array() })
+    }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      User.create({
+        email: req.body.email,
+        password: hash,
+        api_key: uuidv4(),
+        isAdmin: false
+      })
+      .then(user => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(201).send(JSON.stringify({ api_key: user.api_key }))
+      })
+      .catch(error => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(500).send({ error })
+      })
+    });
+  }
+};
--- routes/api/v1/users/index.js
+++ routes/api/v1/users/index.js
@@ -1,11 +1,12 @@
 var router = require('express').Router();
 const { check, validationResult } = require('express-validator');
+const usersController = require('../../../../controllers/api/v1/users_controller.js')
 
 router.post('/', [
   check('email')
     .isEmail(),
   check('password')
     .isLength({ min: 4 })
-], require('./create_user.js'));
+], usersController.create);
 
 module.exports = router;
