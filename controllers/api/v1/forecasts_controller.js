4ca4ec0a4a4d1a9b855619f64d328f8ae8e75115
Vince Carollo <vincecarollo@gmail.com>
2019-08-13 18:52:47 -0600

    Start index action in forecasts controller - checks for correct api_key

--- controllers/api/v1/forecasts_controller.js
+++ controllers/api/v1/forecasts_controller.js
@@ -7,10 +7,16 @@ const forecastIndex = function(req, res, next) {
     }
   })
   .then(user => {
-    res.status(200).send({ user })
+    if (user) {
+
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.status(401).send({ error: 'Not Authorized' })
+    }
   })
   .catch(error => {
-
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({ error })
   })
 }
  module.exports = {
