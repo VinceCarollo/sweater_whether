74f6e8186c05979735baf146e1ecf0f33b18d243
Vince Carollo <vincecarollo@gmail.com>
2019-08-13 19:59:21 -0600

    Add google api call for location data, add dotenv package to hide api keys

--- controllers/api/v1/forecasts_controller.js
+++ controllers/api/v1/forecasts_controller.js
@@ -1,14 +1,32 @@
 var User = require('../../../models').User;
+var https = require('https');
 
-const forecastIndex = function(req, res, next) {
+var forecastIndex = function(req, res, next) {
   User.findOne({
     where: {
       api_key: req.body.api_key
     }
   })
   .then(user => {
+    let lat = '';
+    let lng = '';
     if (user) {
+      https.get(`https://maps.googleapis.com/maps/api/geocode/json?address=denver,co&key=${process.env.GOOGLE_API_KEY}`, (resp) => {
+        let data = '';
 
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+
+        resp.on('end', () => {
+          lat = JSON.parse(data).results[0].geometry.location.lat
+          lng = JSON.parse(data).results[0].geometry.location.lng
+          console.log(lat)
+          console.log(lng)
+        });
+      }).on("error", (err) => {
+        console.log("Error: " + err.message);
+      });
     } else {
       res.setHeader("Content-Type", "application/json");
       res.status(401).send({ error: 'Not Authorized' })
