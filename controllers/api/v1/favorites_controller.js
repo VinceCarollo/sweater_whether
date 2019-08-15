c5b82f9c0aba380e008ff6ddc578ff0718da5405
Vince Carollo <vincecarollo@gmail.com>
2019-08-15 13:35:15 -0600

    Add favorites endpoint that saves user locations in db

--- controllers/api/v1/favorites_controller.js
+++ controllers/api/v1/favorites_controller.js
@@ -9,7 +9,17 @@ var createLocation = function(req, res, next) {
   })
   .then(user => {
     if (user) {
-      res.status(200).send({ user })
+      user.createLocation({
+        name: req.body.location
+      })
+      .then(location => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).send({ message: `${location.name} has been added to your favorites` })
+      })
+      .catch(error => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(500).send({ error })
+      })
     } else {
       res.setHeader("Content-Type", "application/json");
       res.status(401).send({ error: 'Not Authorized' })
